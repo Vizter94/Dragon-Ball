7c18390720a97365da4c4a5baf8224f55aa1ef1f
Krzysiek Zawiasiński <evil753@gmail.com>
2023-05-14 14:21:48 +0200

    Change all site

--- js/script.js
+++ js/script.js
@@ -1,25 +1,11 @@
-console.log("Witaj!");
-let picture = document.querySelector(".picture");
-let name = document.querySelector(".name");
-let body = document.querySelector(".body");
-let button = document.querySelector(".button");
-let characters = document.querySelector(".characters");
-let buttonText = document.querySelector(".buttonText");
+{
+    init = () => {
+        const linkElement=document.querySelector(".js-link");
+        const hideText=document.querySelector(".js-hide");
 
-button.addEventListener("click", () => {
-  characters.classList.toggle("charactersTheme");
-  body.classList.toggle("theme");
-  if (body.classList.contains("theme")) {
-    buttonText.innerText = "jasny"
-  } else {
-    buttonText.innerText = "ciemny"
-  }
-});
-
-name.addEventListener("mouseenter", () => {
-  picture.style.display = "block";
-});
-name.addEventListener("mouseleave", () => {
-  picture.style.display = "none";
-});
-
+        linkElement addEventListener("click", () => {
+            hideText.classList.toggle("section__descriptions--show");
+        })
+    }
+    init();
+}
